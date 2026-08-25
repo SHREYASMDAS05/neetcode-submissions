3d47575ee06bac88ba17e5791074c1e789fe7a95
Shreyas M Das <shreyasmdas18@gmail.com>
2026-08-25 12:27:39 +0530

    Add: permutations-ii - submission-0

---
+++ Data Structures & Algorithms/permutations-ii/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        used = [False] * len(nums)
+        def dfs(curr):
+            if len(nums) == len(curr):
+                res.append(curr.copy())
+                return 
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
+                    continue
+                used[i] = True
+                curr.append(nums[i])
+                dfs(curr)
+                curr.pop()
+                used[i] = False
+
+        dfs([])
+        return res
