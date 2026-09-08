15be64533a788051cd6505f36f5fdd55c907727d
Shreyas M Das <shreyasmdas18@gmail.com>
2026-09-09 00:36:33 +0530

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        '''
+        dp = [-1] * (n+1)
+        def dfs(i):
+            if i >= n:
+                return 0 
+            if dp[i] != -1:
+                return dp[i]
+            dp[i] = max(nums[i] + dfs(i+2) ,dfs(i+1))
+
+            return dp[i]
+
+        return dfs(0)
+        '''
+        #tabulation
+        dp = [0] * (n+2)
+        dp[n] , dp[n+1] = 0 , 0 
+        for i in range(n-1,-1 ,-1):
+            dp[i] = max(nums[i] + dp[i+2] , dp[i+1])
+
+        return dp[0]
