141207a83e6cdc985b655259bfe6529bb23ba859
Shreyas M Das <shreyasmdas18@gmail.com>
2026-09-15 16:38:55 +0530

    Add: longest-common-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-3.py
@@ -0,0 +1,41 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:      
+        '''
+        brute force
+        def gene_subseq(s):
+            res = []
+            def subseq(i,curr):
+                if i == len(s):
+                    res.append(curr.copy())
+                    return
+                curr.append(s[i])
+                subseq(i +1 , curr)
+                curr.pop()
+                subseq(i+1 , curr)
+            subseq(0,[])
+            return res
+        l1 = gene_subseq(text1 )
+        l2 = gene_subseq(text2 )
+        maxi = 0 
+        for i in l1:
+            for j in l2:
+                if i == j and len(i) > maxi:
+                    maxi = len(i)
+        return maxi 
+        '''
+        memo ={}
+        def dfs(idx1 , idx2):
+            if idx1< 0 or idx2< 0 :
+                return 0 
+            if (idx1, idx2) in memo:
+                return memo[(idx1, idx2)]
+            if text1[idx1] == text2[idx2]:
+                memo[(idx1,idx2)] = 1 + dfs(idx1 -1 , idx2 -1 )
+                return 1 + dfs(idx1 -1 , idx2 -1 )
+            else:
+                memo[(idx1,idx2)] = 0 + max(dfs(idx1-1,idx2) , dfs(idx1,idx2-1))
+                return 0 + max(dfs(idx1-1,idx2) , dfs(idx1,idx2-1))
+
+        return dfs(len(text1)-1,len(text2)-1)
+
+
