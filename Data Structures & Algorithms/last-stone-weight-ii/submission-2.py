06a39db4c36108d33d7ec4a68c49e5d2da4422c1
Shreyas M Das <shreyasmdas18@gmail.com>
2026-09-15 18:43:21 +0530

    Add: last-stone-weight-ii - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-2.py
@@ -0,0 +1,49 @@
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        totalsum = sum(stones)
+        '''
+        memo = {}
+        def dfs(i , total):
+            if i == len(stones):
+                return abs(total-(totalsum - total))
+            #min of skipping and notskipping 
+            if (i , total) in memo:
+                return memo[(i , total)]
+            memo[(i , total)] = min(dfs(i+1 , total) , dfs(i + 1 , total + stones[i]))
+            return min(dfs(i+1 , total) , dfs(i + 1 , total + stones[i]))
+        return dfs(0 , 0)
+        
+        target = totalsum // 2
+        dp = [[False] * (target + 1) for i in range(len(stones) +1 )]
+        for i in range(len(stones) + 1):
+            dp[i][0] = True #can get sum 0 
+
+        for i in range(1 ,len(stones) + 1):
+            for j in range(1 , target + 1):
+                #not taking 
+                dp[i][j] = dp[i-1][j]
+                #by taking
+                if stones[i-1] <= j:
+                    dp[i][j] = dp[i][j] or dp[i-1][j - stones[i -1]]    
+        best = 0
+        for i in range(target , -1 , -1):
+            if dp[len(stones)][i] :
+                best = i
+                break
+        return totalsum - 2*best
+        '''
+
+        totalsum = sum(stones)
+        target = totalsum // 2
+
+        dp = [False] * (target + 1)
+        dp[0] = True
+
+        for stone in stones:
+            for j in range(target, stone - 1, -1):
+                dp[j] = dp[j] or dp[j - stone]
+
+        for j in range(target, -1, -1):
+            if dp[j]:
+                return totalsum - 2 * j
+
